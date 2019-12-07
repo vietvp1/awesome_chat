39ab00bc1d1aec426308c38ae8101dc354b79d03
vietvp1 <dienviyeuemvp12@gmail.com>
2019-12-07 18:04:57 +0700

    make it better

--- config/googleAuth.js
+++ config/googleAuth.js
@@ -2,6 +2,6 @@ module.exports = {
     googleAuth: {
         CLIENT_ID: '548526356062-1ldu96qojvoamp7lv9l0nteac8phv4oh.apps.googleusercontent.com',
         CLIENT_SECRET:'lo_z4Aow9wUrsn12ZwFPQEcF',
-        CALLBACK_URL: ' https://awesome-chat-vietanhdev.herokuapp.com/auth/google/callback',
+        CALLBACK_URL: 'https://awesome-chat-vietanhdev.herokuapp.com/auth/google/callback',
     }
 }
